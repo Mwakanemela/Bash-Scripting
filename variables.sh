17a00dfbfa67dbf4297d8aa16f72320d8b37b958
Mwakanemela <kayangemwakanemela@gmail.com>
2024-09-23 08:06:13 -0400

    variables - bash scripting

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+myname="Mwakanemela"
+
+workingdir=$(pwd)
+
+now=$(date)
+
+echo "My name is $myname"
+echo "Working directory is $workingdir"
+echo "The system time and and date is: $now"
+
+
+echo "Your username is : $USER"
